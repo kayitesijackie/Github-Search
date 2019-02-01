0f9a181b446fccd0c637c171fd6a6acbebbc8b1f
Kayitesi Jacqueline <kayitesijacqueline@gmail.com>
2019-02-01 11:22:17 +0200

    add class ProfileService

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import 'rxjs/add/operator/map';
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' //we declare that this service should be created by the root application injector.
 })
+
 export class ProfileService {
 
-  constructor() { }
+  private username: string;
+  constructor(private http:HttpClient) { 
+    console.log ("service is now ready!");
+    this.username = 'kayitesijackie';
+  }
+  getProfileInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username)
+
+  }
 }
