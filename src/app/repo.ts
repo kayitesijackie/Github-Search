ffbe064a223271bb1dca7d65c3a8c47cb687712e
kayitesijackie <bazimyas@gmail.com>
2019-02-05 16:29:50 +0200

    remove argument

--- src/app/repo.ts
+++ src/app/repo.ts
@@ -1,3 +1,3 @@
 export class Repo {
-    constructor(public name: string, public description: string, public html_url: string, public  clone_url: string, public homepage:string ) {}
+    constructor(public name: string, public description: string, public html_url: string, public  clone_url: string, public homepage:string, public apiRepokey: string) {}
     }
