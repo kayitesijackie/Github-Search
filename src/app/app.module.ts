ca5f66cacd8fae2f279235ba76087ed1f41f15dd
kayitesijackie <bazimyas@gmail.com>
2019-02-05 16:24:08 +0200

    import RouterModule and add const routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,32 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProfileService } from './services/profile.service';
 import { RouterModule, Routes } from '@angular/router';
 
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ProfileFormComponent } from './profile-form/profile-form.component';
 import { StrikethroughDirective } from './strikethrough.directive';
+import { RepositoriesComponent } from './repositories/repositories.component';
 
 
-const routes:Routes=[]
+
+const routes:Routes=[
+  {path: 'profile', component: ProfileComponent},
+  {path: 'repositories', component: RepositoriesComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
     ProfileFormComponent,
-    StrikethroughDirective
+    StrikethroughDirective,
+    RepositoriesComponent,
+   
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(routes)
 
   ],
