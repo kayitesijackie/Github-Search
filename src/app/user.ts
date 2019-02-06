d4a6c89b3a6cd028c7f8fa6e83fc88a5af3c4b7b
kayitesijackie <bazimyas@gmail.com>
2019-02-06 17:33:00 +0200

    add argument datecount

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,5 +1,5 @@
 export class User {
-    constructor(public login: string, public html_url: string, public public_repos:number) {
+    constructor(public login: string, public html_url: string, public public_repos:number,public dateCount:Date, public avatar_url: string, public followers: number, public following: number) {
     }
   
 }
